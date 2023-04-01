bf6f641d8f4afe91d2c39be8008dcf64374599bf
whguddnr9 <78585609+whguddnr9@users.noreply.github.com>
2023-04-01 16:41:50 +0900

    remove the cache

---
+++ ch14/ex14.2/ex14.2.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	b := 20
+
+	var p1 *int = &a
+	var p2 *int = &a
+	var p3 *int = &b
+
+	fmt.Printf("p1 == p2 : %v\n", p1 == p2)
+	fmt.Printf("p2 == p3 : %v\n", p2 == p3)
+
+}
