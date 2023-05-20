378daad16820c12ee9c697fc1a474c2221c32976
whguddnr9 <78585609+whguddnr9@users.noreply.github.com>
2023-05-20 17:59:53 +0900

    append()를 이용한 코드 개선

--- ch18/ex18.8/ex18.8.go
+++ ch18/ex18.8/ex18.8.go
@@ -4,12 +4,15 @@ import "fmt"
 
 func main() {
 	slice1 := []int{1, 2, 3, 4, 5}
-
+	/* 아래와 같이 선언하면 for 구문으로 반복해야하는 단점이 생김
 	slice2 := make([]int, len(slice1))
 
 	for i, v := range slice1 {
 		slice2[i] = v
 	}
+	*/
+
+	slice2 := append([]int{}, slice1...)
 
 	slice1[1] = 100
 	fmt.Println(slice1)
